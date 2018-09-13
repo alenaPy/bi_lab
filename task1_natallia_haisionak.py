5dcb757a01d2d96afadb8989d651d1fde26bb1f6
Natallia Haisionak <natallia_haisionak@epam.com>
2018-09-13 13:20:08 +0300

    #1 natallia haisionak

---
+++ task1_natallia_haisionak.py
@@ -0,0 +1,48 @@
+"""labwork1 Natallia Haisionak."""
+import re
+import sys
+# 1
+print('Hello, world!')
+# 2
+name = input('What is your name?\n')
+print('Hi, %s.' % name)
+# 3
+friends = ['john', 'pat', 'gary', 'michael']
+for i, name in enumerate(friends):
+    print("iteration {iteration} is {name}".format(iteration=i, name=name))
+# 4
+parents, babies = (1, 1)
+while babies < 100:
+    print('This generation has {0} babies'.format(babies))
+    parents, babies = (babies, parents + babies)
+# 5
+
+
+def greet(name):
+    """Function."""
+    print('Hello', name)
+
+
+greet('Jack')
+greet('Jill')
+greet('Bob')
+# 6
+for test_string in ['555-1212', 'ILL-EGAL']:
+    if re.match(r'^\d{3}-\d{4}$', test_string):
+        print(test_string, 'is a valid US local phone number')
+    else:
+        print(test_string, 'rejected')
+# 7
+prices = {'apple': 0.40, 'banana': 0.50}
+my_purchase = {
+    'apple': 1,
+    'banana': 6}
+grocery_bill = sum(prices[fruit] * my_purchase[fruit]
+                   for fruit in my_purchase)
+print('I owe the grocer $%.2f' % grocery_bill)
+# 8 This program adds up integers in the command line
+try:
+    total = sum(int(arg) for arg in sys.argv[1:])
+    print('sum =', total)
+except ValueError:
+    print('Please supply integer arguments')
