41f376619d524d35a04089516c99f1d8fa39a649
Artsiom Barysionak <mr.tyomich@gmail.com>
2018-09-13 13:28:33 +0300

    Homework1: Artsiom Barysionak

--- Artsiom_Barysionak/Homework1/test10.py
+++ Artsiom_Barysionak/Homework1/test10.py
@@ -6,14 +6,16 @@ activities = {8: 'Sleeping',
               17: 'Working',
               18: 'Commuting',
               20: 'Eating',
-              22: 'Resting' }
+              22: 'Resting'}
 
 time_now = localtime()
 hour = time_now.tm_hour
 
 for activity_time in sorted(activities.keys()):
     if hour < activity_time:
-        print(activities[activity_time])  # print command was edited according to Python 3.0 conventions
+        print(activities[activity_time])
+        # print command was edited according to Python 3.0 conventions
         break
 else:
-    print('Unknown, AFK or sleeping!')  # print command was edited according to Python 3.0 conventions
+    print('Unknown, AFK or sleeping!')
+    # print command was edited according to Python 3.0 conventions
