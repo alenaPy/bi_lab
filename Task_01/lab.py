8ea2e46a104e8adbd397543bdbbc2ef901c22790
Aleh Salei <Aleh_Salei@epam.com>
2018-09-13 13:38:05 +0300

    lab.py fixed

--- Task_01/lab.py
+++ Task_01/lab.py
@@ -12,7 +12,7 @@ def validate(queens):
     """Queen validation."""
     left = right = col = queens[-1]
     for r in reversed(queens[:-1]):
-        left, right = left-1, right+1
+        left, right = left - 1, right + 1
         if r in (left, col, right):
             raise BailOut
 
@@ -37,4 +37,4 @@ queens = add_queen([])
 print(queens)
 
 # print "\n".join(". "*q + "Q " + ". "*(BOARD_SIZE-q-1) for q in queens)
-print("\n".join(". "*q + "Q " + ". "*(BOARD_SIZE-q-1) for q in queens))
+print("\n".join(". " * q + "Q " + ". " * (BOARD_SIZE - q - 1) for q in queens))
