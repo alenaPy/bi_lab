426ed1dbf399368dbb42469efad20c70ec628a0a
Natallia Baranava <Natallia_Baranava@epam.com>
2018-09-14 17:13:57 +0300

    fineshed homework1

--- task2/2_1.py
+++
@@ -1,9 +0,0 @@
-"""Determine if string is a palindrome."""
-import re
-string = input('Input row: ')
-test = re.compile('\w|\d')
-treated_string = test.findall(string.lower())
-if treated_string == treated_string[::-1]:
-    print("It is palindrome")
-else:
-    print("It is not palindrome")
