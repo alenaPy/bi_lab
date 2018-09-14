aa3734c56013a9583e0fe0514037e4722ae0a457
Nataliya Blashuk <nataliya_blashuk@epam.com>
2018-09-14 16:02:53 +0300

    Modified fizz_buzz.py

--- Nataliya Blashuk/Task_02/fizz_buzz.py
+++ Nataliya Blashuk/Task_02/fizz_buzz.py
@@ -1,10 +1,10 @@
 """FizzBuzz."""
 for i in range(100):
-    if (i+1) % 3 == 0 and i % 5 == 0:
+    if (i + 1) % 3 == 0 and i % 5 == 0:
         print("FizzBuzz")
-    elif (i+1) % 5 == 0:
+    elif (i + 1) % 5 == 0:
         print("Buzz")
-    elif (i+1) % 3 == 0:
+    elif (i + 1) % 3 == 0:
         print("Fizz")
     else:
-        print(i+1)
+        print(i + 1)
