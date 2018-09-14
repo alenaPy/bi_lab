e7e071cd076188a1f8d0e354b9b528f20ba5cb86
Aryna Kandrashevich <Aryna_Kandrashevich@epam.com>
2018-09-14 15:12:33 +0300

    Labwork2: Tasks 1-3

---
+++ task2/task2(calculation).py
@@ -0,0 +1,7 @@
+dollar = int(input('Please enter dollars\n'))
+cent = int(input('Please enter cents\n'))
+items = int(input('Please enter items\n'))
+
+dollar = int(dollar)*100
+sum = ((int(dollar)+int(cent))*items)/100
+print('Total cost: %.2f' % sum)
---
+++ task2/task2(palindrome).py
@@ -0,0 +1,17 @@
+s = input('Enter string\n')
+
+l = len(s)
+
+for char in s:
+    if char in " ?.!/;:":
+        s = (s.replace(char, '').lower())
+else:
+    s = (s.lower())
+
+for i in range(l//2):
+    if s[i] != s[-1-i]:
+        print("It's not palindrome %s" % s)
+        quit()
+
+print("It's palindrome %s" % s)
+
