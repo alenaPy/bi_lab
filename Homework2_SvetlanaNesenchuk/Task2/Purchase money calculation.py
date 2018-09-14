e93678a416c9030629fd6db4081c1f3e716851e6
Svetlana Nesenchuk <svetlana_nesenchuk@epam.com>
2018-09-14 18:37:59 +0300

    added task2

---
+++ Homework2_SvetlanaNesenchuk/Task2/Purchase money calculation.py
@@ -0,0 +1,15 @@
+"""Purchase money calculation."""
+
+dollars = int(input('Please enter dollars:'))
+cents = int(input('Please enter cents:'))
+items = int(input('Please enter the number of items:'))
+
+
+def total_cost(dollars, cents, items):
+    """Total cost calculation."""
+    total_dollars = int((dollars * 100 + cents) * items // 100)
+    total_cents = int((dollars * 100 + cents) * items % 100)
+    print("Total cost is %d dollars and %d cents" % (total_dollars, total_cents))
+
+
+total_cost(dollars, cents, items)
