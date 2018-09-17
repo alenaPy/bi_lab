c865fc7b90ae8a1635e5d644e3d47966463d3297
Veranika Simanenka <Veranika_Simanenka@epam.com>
2018-09-17 21:35:43 +0300

    refactored the code

--- labwork2/Task4/Correct Sentence.py
+++ labwork2/Task4/Correct Sentence.py
@@ -10,8 +10,9 @@ Output: A string."""
 def correct_sentence(text: str) -> str:
     """Returns a corrected sentence which starts with a capital letter
     and ends with a dot."""
+    
     # your code here
     text = text[0].upper() + text[1:]
     if text[len(text) - 1] != '.':
-        text = text+'.'
+        text = text + '.'
     return text
