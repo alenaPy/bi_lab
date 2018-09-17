cbc7a651d60159cde9363089d6319422b2e465ae
Gleb Salnikov <gleb_salnikov@epam.com>
2018-09-18 00:06:57 +0300

    [task_03 #2] runner.py is edited

--- task3/runner.py
+++ task3/runner.py
@@ -1,12 +1,20 @@
 """Functions runner module."""
+from inspect import getmembers, isfunction
 import pytasks
 
 
 def runner(*funcs):
     """Run functions implemented in pytasks module."""
-    for func in funcs:
-        print(func())
+    meta = [obj for obj in getmembers(pytasks) if isfunction(obj[1])]
+    all_funcs = list(meta[i][0] for i in range(len(meta)))
+
+    if len(funcs) == 0:
+        res = all_funcs
+    else:
+        res = funcs
+    for func in res:
+        print(eval('pytasks.' + func + '()'))
 
 
 # example
-runner(pytasks.generate_numbers, pytasks.fizzbuzz)
+runner('fizzbuzz')
