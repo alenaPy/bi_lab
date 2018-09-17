fcc7d355ff0e5b3c7461ee593105423295060534
Aryna Kandrashevich <Aryna_Kandrashevich@epam.com>
2018-09-17 14:33:40 +0300

    Check errors

--- task2/checkio(2).py
+++ task2/checkio(2).py
@@ -1,12 +1,15 @@
 def correct_sentence(text: str) -> str:
 
     """ returns a corrected sentence which starts with a capital letter and ends with a dot.
-        For the input of your function will be given one sentence. You have to return its fixed copy in a way so it’s
+        For the input of your function will be given one sentence. You have to return its
+         fixed copy in a way so it’s
         always starts with a capital letter and ends with a dot.
-Pay attention to the fact that not all of the fixes is necessary. If a sentence already ends with a dot then adding
+Pay attention to the fact that not all of the fixes is necessary. If a sentence already
+ends with a dot then adding
 another one will be a mistake.
 Input:  A string.
-Output:  A string. """
+Output:  A string.
+"""
 
     text = text[0].upper() + text[1:]
 
