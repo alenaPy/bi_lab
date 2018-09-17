f8c905f34fd17ece1dd2e531a9159934bf17856a
Aryna Kandrashevich <Aryna_Kandrashevich@epam.com>
2018-09-17 14:47:10 +0300

    Upload tasks from site try again

--- task2/checkio(5).py
+++ task2/checkio(5).py
@@ -24,7 +24,8 @@ print(between_markers('What is >apple<', '>', '<'))
 
 
 assert between_markers('What is >apple<', '>', '<') == "apple", "One sym"
-assert between_markers("<head><title>My new site</title></head>", "<title>", "</title>") == "My new site", "HTML"
+assert between_markers("<head><title>My new site</title></head>",
+                       "<title>", "</title>") == "My new site", "HTML"
 assert between_markers('No[/b] hi', '[b]', '[/b]') == 'No', 'No opened'
 assert between_markers('No [b]hi', '[b]', '[/b]') == 'hi', 'No close'
 assert between_markers('No hi', '[b]', '[/b]') == 'No hi', 'No markers at all'
