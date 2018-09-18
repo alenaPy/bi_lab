e82c57fa4794f741bb847e9419800b67fd372076
Veranika Simanenka <Veranika_Simanenka@epam.com>
2018-09-18 16:41:17 +0300

    labwork4: Added script for Task1:Read and save data practice

---
+++ labwork4/imdb.py
@@ -0,0 +1,46 @@
+"""Read and save data practice."""
+
+titles = []
+rates = []
+years = []
+
+def count_characters(count_me_string):
+    """Count and return the numbers of each character in the str argument."""
+    dict2 = {i: sum(x is i for x in count_me_string) for i in
+             set(list(count_me_string))}
+    return dict2
+
+try:
+    with open('ratings.list', 'r', encoding='ISO-8859-1') as dbdata:
+        for i, line in enumerate(dbdata):
+
+            if i >= 28 and i < 278:
+
+                titles.append(line[:line.find('(')].split('  ')[6])
+                rates.append(line.split('  ')[5])
+                years.append(line[line.find('(') + 1:line.find(')')])
+
+except FileNotFoundError:
+    print('This file is not found! \n')
+
+
+with open('titles.txt', 'w', encoding = 'UTF-8') as ttl:
+    for x in titles:
+        ttl.write(x+'\n')
+
+with open('rates.txt', 'w', encoding = 'UTF-8') as rts:
+
+    dict1 = dict((x,rates.count(x)) for x in set(rates))
+    for x in dict1:
+        rts.write(x+' '+ str(dict1[x])+'\n')
+
+with open('years.txt', 'w', encoding = 'UTF-8') as yrs:
+
+    dict2 = dict((x, years.count(x)) for x in set(years))
+
+    for x in dict2:
+        yrs.write(x+' '+ str(dict2[x])+'\n')
+
+
+
+
