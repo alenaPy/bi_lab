fd38656632e9cb8617a32f9e22258de730817f6e
Veranika Simanenka <Veranika_Simanenka@epam.com>
2018-09-18 16:46:45 +0300

    labwork4: Added script for Task1:Read and save data practice

--- labwork4/imdb.py
+++ labwork4/imdb.py
@@ -21,17 +21,17 @@ except FileNotFoundError:
 
 with open('titles.txt', 'w', encoding='UTF-8') as ttl:
     for x in titles:
-        ttl.write(x+'\n')
+        ttl.write(x + '\n')
 
 with open('rates.txt', 'w', encoding='UTF-8') as rts:
 
     dict1 = dict((x, rates.count(x)) for x in set(rates))
     for x in dict1:
-        rts.write(x+ ' ' + str(dict1[x])+'\n')
+        rts.write(x + ' ' + str(dict1[x]) + '\n')
 
 with open('years.txt', 'w', encoding='UTF-8') as yrs:
 
     dict2 = dict((x, years.count(x)) for x in set(years))
 
     for x in dict2:
-        yrs.write(x+ ' ' + str(dict2[x])+'\n')
+        yrs.write(x + ' ' + str(dict2[x]) + '\n')
