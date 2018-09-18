ba45c38c5ef2037530b048e73d02e6e6f5c59307
natalliahaisionak <pr.cherry@mail.ru>
2018-09-19 00:51:28 +0300

    Homework4: Natallia Haisionak part2

--- fuel_station.py
+++ fuel_station.py
@@ -15,20 +15,25 @@ class Dispenser:
         """Add fuel."""
         if (litres + self.fuel_balance) <= self.capacity:
             self.fuel_balance += litres
-            print("Refuel dispenser number " + str(self.num) + " with " + str(litres) +
+            print("Refuel dispenser number " + str(self.num) + " with " + str(
+                litres) +
                   " litres. Total fuel balance = " + str(self.fuel_balance))
         else:
-            print("Dispenser number: " + str(self.num) + " can be refueled only by " +
+            print("Dispenser number: " + str(
+                self.num) + " can be refueled only by " +
                   str(self.capacity - self.fuel_balance) + " litres")
 
     def refill_car(self, litres):
         """Refill car."""
         if (self.fuel_balance - litres) >= 0:
             self.fuel_balance -= litres
-            print("Refill car from dispenser number " + str(self.num) + " with " + str(
-                litres) + " litres. Total fuel balance = " + str(self.fuel_balance))
+            print("Refill car from dispenser number " + str(
+                self.num) + " with " + str(
+                litres) + " litres. Total fuel balance = " + str(
+                self.fuel_balance))
         else:
-            print("Dispenser number: " + str(self.num) + " - not enough fuel.  Available " +
+            print("Dispenser number: " + str(
+                self.num) + " - not enough fuel.  Available " +
                   str(self.fuel_balance) + " litres")
 
 
