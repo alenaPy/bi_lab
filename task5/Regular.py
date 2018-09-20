69987d5b8d8910530ef6d7d26be39e6ae345a752
Aryna Kandrashevich <Aryna_Kandrashevich@epam.com>
2018-09-20 16:46:45 +0300

    Upload task5 again

--- task5/Regular.py
+++ task5/Regular.py
@@ -1,13 +1,15 @@
 """Regular Expressions."""
 import re
 # find all email addresses in string.
-string_1 = 'I have two emails one of this is arina.kondrashevich@mail.ru and the other one is beliashik97@mail.ru '
+string_1 = 'I have two emails one of this is arina.kondrashevich@mail.ru' \
+           ' and the other one is beliashik97@mail.ru '
 emails = re.findall(r'\w+@\w+\.\w+', string_1)
 print(emails)
 
 
 # find all 3, 4, 5 characters long words in a string.
-string_2 = 'Hello! My name is Aryna. I''m student of Belarusian state university of informatics and radio electronics '
+string_2 = 'Hello! My name is Aryna. I''m student of Belarusian state' \
+           ' university of informatics and radio electronics '
 words = re.findall(r'\w+', string_2)
 for i in words:
     if len(i) in (3, 4, 5):
