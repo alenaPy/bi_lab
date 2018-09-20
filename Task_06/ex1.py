fd9a7c40d8a9836e70255dd321128189ea1c5962
Gleb Salnikov <gleb_salnikov@epam.com>
2018-09-20 19:25:51 +0300

    [task_05 #4] ex1.py arguments implementation is added

--- Task_06/ex1.py
+++ Task_06/ex1.py
@@ -1,5 +1,6 @@
 ﻿"""Ex1."""
 
+import argparse
 import csv
 import gzip
 import io
@@ -8,29 +9,62 @@ import os
 import requests
 import zipfile
 
-# zip file
-exists = os.path.isfile('FL_insurance_sample.csv')
-if not exists:
-    zip_url = 'http://spatialkeydocs.s3.amazonaws.com/' \
-              'FL_insurance_sample.csv.zip'
-    zip_get = requests.get(zip_url)
-    data = zipfile.ZipFile(io.BytesIO(zip_get.content))
-    data.extractall()
-    data.close()
+parser = argparse.ArgumentParser(description="Choose download type")
 
-list = []
-with open('FL_insurance_sample.csv') as csvfile:
-    csv_rd = csv.reader(csvfile)
+parser.add_argument('--type', help='zip - load .zip file, gzip - gzip, '
+                                   'no args - both')
 
-    for row in csv_rd:
-        list.append(row)
-        print(', '.join(row))
-print('=' * 200)
+arg = parser.parse_args()
 
-# gzipped json file
-gz_url = 'https://wiki.mozilla.org/images/f/ff/Example.json.gz'
-gz_get = requests.get(gz_url)
-with gzip.open(io.BytesIO(gz_get.content), 'r') as data:
-    file = json.loads(data.read())
+if arg.type == 'zip':
+    # zip file
+    exists = os.path.isfile('FL_insurance_sample.csv')
+    if not exists:
+        zip_url = 'http://spatialkeydocs.s3.amazonaws.com/' \
+                  'FL_insurance_sample.csv.zip'
+        zip_get = requests.get(zip_url)
+        data = zipfile.ZipFile(io.BytesIO(zip_get.content))
+        data.extractall()
+        data.close()
 
-print(file)
+    list = []
+    with open('FL_insurance_sample.csv') as csvfile:
+        csv_rd = csv.reader(csvfile)
+        for row in csv_rd:
+            list.append(row)
+            print(', '.join(row))
+    print('=' * 200)
+
+if arg.type == 'gzip':
+    # gzipped json file
+    gz_url = 'https://wiki.mozilla.org/images/f/ff/Example.json.gz'
+    gz_get = requests.get(gz_url)
+    with gzip.open(io.BytesIO(gz_get.content), 'r') as data:
+        file = json.loads(data.read())
+    print(file)
+
+if not arg.type:
+    # zip file
+    exists = os.path.isfile('FL_insurance_sample.csv')
+    if not exists:
+        zip_url = 'http://spatialkeydocs.s3.amazonaws.com/' \
+                  'FL_insurance_sample.csv.zip'
+        zip_get = requests.get(zip_url)
+        data = zipfile.ZipFile(io.BytesIO(zip_get.content))
+        data.extractall()
+        data.close()
+
+    list = []
+    with open('FL_insurance_sample.csv') as csvfile:
+        csv_rd = csv.reader(csvfile)
+        for row in csv_rd:
+            list.append(row)
+            print(', '.join(row))
+    print('=' * 200)
+
+    # gzipped json file
+    gz_url = 'https://wiki.mozilla.org/images/f/ff/Example.json.gz'
+    gz_get = requests.get(gz_url)
+    with gzip.open(io.BytesIO(gz_get.content), 'r') as data:
+        file = json.loads(data.read())
+    print(file)
