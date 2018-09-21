6926224437fde078396933249527bf2fa1ecc99d
Gleb Salnikov <gleb_salnikov@epam.com>
2018-09-21 15:37:15 +0300

    [task_07 #1] a file and its tests are added

---
+++ Task_07/pytasks.py
@@ -0,0 +1,25 @@
+"""Module that contains definition for functions from previous tasks."""
+
+
+def generate_numbers(n=20):
+    """Return a dictionary, keys ∈ [1, arg], values are square of keys."""
+    return {i: i**2 for i in range(1, int(n) + 1)}
+
+
+def count_characters(arg=''):
+    """Count and return the numbers of each character in arg."""
+    return dict(zip(arg, list(map(lambda x: arg.count(x), arg))))
+
+
+def is_palindrome(arg=''):
+    """Find palindrome."""
+    import re
+
+    # removing all non-alphanumeric symbols
+    mod_in_string = re.sub(r'\W+', '', arg)
+
+    # checking whether the string is a palindrome
+    if mod_in_string.lower() == mod_in_string[::-1].lower():
+        return True
+    else:
+        return False
