a1100a6c0713e368bc4118518545b646a7195cfe
Yauheni Bandarchyk <Yauheni_Bandarchyk@epam.com>
2018-09-23 18:06:19 +0300

    Task6. Added serializations and argparse

--- task6/Task.py
+++ task6/Task.py
@@ -1,30 +1,102 @@
 """Task6."""
 
-from csv import reader
-import gzip
-from io import BytesIO
+import argparse
+import csv
 import json
-from os import path
-from requests import get
-from zipfile import ZipFile
+import io
+import os
+import gzip
+import requests
+import urllib.request
+import xml.etree.cElementTree as ET
+import yaml
+import zipfile
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--download", help="Provide downloading of files",
+                    action="store_true")
+parser.add_argument("--format",
+                    help='Use json | xml | csv | yaml'
+                    ' to convert to choosen format')
+
+arguments = parser.parse_args()
 
 
-exists = path.isfile("countries of the world.csv.csv")
+exists = os.path.isfile('arch.zip')
 
-if not exists:
-    request = get("https://github.com/ZhenyaBond/123/raw/master/arch.zip")
-    zip_file = ZipFile(BytesIO(request.content))
-    zip_file.extractall()
-    zip_file.close()
+
+def get_zip():
+    """Download zip archive."""
+    urllib.request.urlretrieve(
+        'https://github.com/ZhenyaBond/123/raw/master/arch.zip', 'arch.zip')
+    print("Downloaded successfully.")
+
+
+def read_zip():
+    zip_f = zipfile.ZipFile('arch.zip')
+    zip_f.extractall()
+    zip_f.close()
 
     with open("countries of the world.csv", 'r') as csvfile:
-        csvreader = reader(csvfile)
+        csvreader = csv.reader(csvfile)
         for row in csvreader:
             print(row)
 
-gz_url = 'https://wiki.mozilla.org/images/f/ff/Example.json.gz'
-gz_get = get(gz_url)
-with gzip.open(BytesIO(gz_get.content), 'r') as data:
-    file = json.loads(data.read())
 
-print("This is content of JSON file: {0}".format(file))
+def get_gz():
+    """Read json data."""
+    gz_url = 'https://wiki.mozilla.org/images/f/ff/Example.json.gz'
+    gz_get = requests.get(gz_url)
+    with gzip.open(io.BytesIO(gz_get.content), 'r') as data:
+        file = json.loads(data.read())
+    print("This is content of JSON file: {0}".format(file))
+
+
+if arguments.download:
+    if not exists:
+        get_zip()
+    else:
+        print("File exists.")
+
+if arguments.format == 'json':
+    with open('countries of the world.csv') as f:
+        reader = csv.DictReader(f)
+        rows = list(reader)
+
+    with open('json_format.json', 'w') as f:
+        json.dump(rows, f)
+    print("json created.")
+
+if arguments.format == 'xml':
+    with open('countries of the world.csv') as f:
+        text = f.readlines()
+
+    root = ET.Element('root')
+    root.text = str(text)
+    tree = ET.ElementTree(root)
+    tree.write('xml-format.xml')
+    print("xml-created.")
+
+if arguments.format == 'csv':
+    with open('countries of the world.csv') as f:
+        csvreader = csv.reader(f)
+        text = []
+        for row in csvreader:
+            text += row
+
+    with open('csv-format.csv', 'w') as csv_f:
+        csv_writer = csv.writer(csv_f)
+        csv_writer.writerow(text)
+    print("scv created.")
+
+if arguments.format == 'yaml':
+    with open('countries of the world.csv') as csv_f:
+        csvreader = csv.reader(csv_f)
+        text = []
+        for row in csvreader:
+            text += row
+
+    with open('yaml-format.yaml', 'w') as f:
+        yaml.dump(text, f, default_flow_style=False)
+    print('yaml created.')
