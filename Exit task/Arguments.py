739218cc17200d672fbc10e344bd1124a3600461
Aryna Kandrashevich <Aryna_Kandrashevich@epam.com>
2018-09-24 13:23:14 +0300

    Upload finished exit task

--- Exit task/Arguments.py
+++ Exit task/Arguments.py
@@ -1,4 +1,13 @@
 import argparse
+from collections import OrderedDict
+import csv
+import io
+import json
+import os
+import requests
+import yaml
+import zipfile
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--year', help='displays Top250 movies titles with year',
                     default=False)
@@ -9,6 +18,9 @@ parser.add_argument('--histogram', help='displays histogram for rating or for'
                     ' years (in text format)', default=False)
 parser.add_argument('--output', help='stores all data to specified filename'
                     ' file', default=False)
+parser.add_argument('--format', help='convert to another format'
+                    , default=False)
+
 arguments = parser.parse_args()
 
 s = 'New  Distribution  Votes  Rank  Title'
@@ -97,3 +109,97 @@ if arguments.output:
         out_file.write((str(i) + '. ' + movie[0] + ' ' + movie[2] + ' ' +
                         movie[1]) + '\n')
     out_file.close()
+
+
+csv_file = 'channels.csv'
+
+
+# zip file
+
+exists = os.path.isfile(csv_file)
+
+if not exists:
+    try:
+        zip_url = 'https://github.com/arinakondrashevich97/bi_lab/raw/exit_task/Exit%20task/channels.zip'
+        zip_get = requests.get(zip_url)
+        data = zipfile.ZipFile(io.BytesIO(zip_get.content))
+        data.extractall()
+        data.close()
+        print('zip archive extracted')
+    except zipfile.BadZipfile:
+        print('BadZipfile: File is not a zip file')
+else:
+    print('data file ' + csv_file + ' exists')
+
+
+# to csv
+if arguments.format == 'csv':
+    with open(csv_file) as file:
+        csv_rd = csv.reader(file)
+        with open('csv_output.csv', 'w', newline='') as write_file:
+            writer = csv.writer(write_file)
+            for row in csv_rd:
+                writer.writerow(row)
+
+
+# to JSON
+if arguments.format == 'json':
+    fieldnames = (
+        "id", "CHANNEL_NAME", "CHANNEL_DESC")
+    entries = []
+    with open(csv_file, 'r') as csvfile:
+        reader = csv.DictReader(csvfile, fieldnames)
+        for row in reader:
+            entry = OrderedDict()
+            for field in fieldnames:
+                entry[field] = row[field]
+            entries.append(entry)
+    output = {"Service": entries}
+    with open('file.json', 'w') as jsonfile:
+        json.dump(output, jsonfile)
+        jsonfile.write('\n')
+
+# to XML
+if arguments.format == 'xml':
+    csvData = csv.reader(open(csv_file))
+    xmlData = open('xmlData.xml', 'w')
+    xmlData.write('<?xml version="1.0"?>' + "\n")
+    xmlData.write('<csv_data>' + "\n")
+    rowNum = 0
+    for row in csvData:
+        if rowNum == 0:
+            tags = row
+        else:
+            xmlData.write('<row>' + "\n")
+            for i in range(len(tags)):
+                xmlData.write('    ' + '<' + tags[i] + '>' +
+                              row[i] + '</' + tags[i] + '>' + "\n")
+            xmlData.write('</row>' + "\n")
+        rowNum += 1
+    xmlData.write('</csv_data>' + "\n")
+    xmlData.close()
+
+# to yaml
+if arguments.format == 'yaml':
+    in_file = open(csv_file, "r")
+    out_file = open('yaml_file.yaml', "w")
+    items = []
+
+    def convert_to_yaml(line):
+        """Convert to yaml."""
+        item = {
+            'id': line[0],
+            'CHANNEL_NAME': line[1],
+            'CHANNEL_DESC': line[2]
+        }
+        items.append(item)
+    try:
+        reader = csv.reader(open(csv_file, "r"))
+        # skip headers
+        next(reader)
+        for i, line in enumerate(reader):
+            convert_to_yaml(line)
+        out_file.write(yaml.dump(items, default_flow_style=False))
+    finally:
+        in_file.close()
+        out_file.close()
